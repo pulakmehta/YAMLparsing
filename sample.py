c48046de8fb73e56491a7df0185458a5c8162cf1
pulakmehta <32372584+pulakmehta@users.noreply.github.com>
2021-04-12 02:29:07 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -6,8 +6,8 @@ import os
 class Test(unittest.TestCase):
 
     def test_should_correctly_merge_single_yaml_file(self):
-        test1 = os.path.normpath(os.path.join(dirname,r'Testcase\1\file','test.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,r'Testcase\expected','1.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'Testcase/1/file','test.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'Testcase/expected','1.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(mergingscript(test1).main(), documents)
