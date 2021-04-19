a078e5740053de188130089c8088c930db2b8876
pulakmehta <32372584+pulakmehta@users.noreply.github.com>
2021-04-19 09:21:57 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -20,58 +20,58 @@ import os
 
 class Test(unittest.TestCase):
     def test_fail_invalid_yaml(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/0','test0.docx'))
+        test1 = os.path.normpath(os.path.join(dirname,'TestCase\0','test0.docx'))
         self.assertRaises(TypeError,MergingScript(test1).main())
 
     def test_fail_invalid_dir(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/0/test/test','test0.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\0\test\test','test0.yaml'))
         self.assertRaises(TypeError,MergingScript(test1).main())
         
     def test_given_int_and_string(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/1/test','test1.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,'TestCase/expected','1.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\1\test','test1.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','1.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
 
     def test_given_yaml_lists(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/2/test','test2.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,'TestCase/expected','2.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\2\test','test2.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','2.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
 
     def test_given_nested_key_value(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/3/test','test3.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,'TestCase/expected','3.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\3\test','test3.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','3.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
 
     def test_sister_folder_present(self):
-        test1 = os.path.normpath(os.path.join(dirname,r'TestCase/4/test/test','test4.yaml'))
-        test1_ans = (os.path.join(dirname,'TestCase/expected','4.yaml')
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\4\test\test','test4.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','4.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
 
     def test_complex(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/5/test','test5.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,'TestCase/expected','5.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\5\test','test5.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','5.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
 
     def test_empty_file_inner(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/6/test/test','test6.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,'TestCase/expected','6.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\6\test\test','test6.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','6.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
 
     def test_empty_file_outer(self):
-        test1 = os.path.normpath(os.path.join(dirname,'TestCase/7/test/test','test7.yaml'))
-        test1_ans = os.path.normpath(os.path.join(dirname,'TestCase/expected','7.yaml'))
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\7\test\test','test7.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','7.yaml'))
         with open(test1_ans) as file:
             documents = yaml.load(file, Loader=yaml.FullLoader)
         self.assertEqual(MergingScript(test1).main(), documents)
