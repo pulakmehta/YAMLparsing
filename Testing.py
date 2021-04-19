2920c0c8f1ae804ee5eec44ce496135f312bab7d
pulakmehta <32372584+pulakmehta@users.noreply.github.com>
2021-04-19 06:23:36 -0700

    Build

--- Testing.py
+++ Testing.py
@@ -1,84 +1,59 @@
-import yaml # Please ensure you have PyYAML installed. This script has been tested for v5.3.1
-import os.path
-import sys
-from os import path
+from scriptYaml import MergingScript
+import yaml
+import unittest
+import os
 
-def printing(yamlFile):
-    print("5")
-    yaml.dump(yamlFile,sys.stdout)
-
-def merge(a,b):
-    try:
-        if isinstance(a, list):
-            # lists can be only appended
-            if isinstance(b, list):
-                a.extend(b)
-            else:
-                a.append(b)
-        elif isinstance(a, dict):
-            # dicts must be merged
-            if isinstance(b, dict):
-                for key in b:
-                    if key in a:
-                        a[key] = merge(a[key], b[key])
-                    else:
-                        a[key] = b[key]
-    except:
-        print("Error in merging")
-        return()
-    return a
-
-
-def merging(fileList):
-    num = len(fileList)
-    i = 0
-    temp = 0
-    with open(fileList[0]) as file:
-        fromYaml = yaml.load(file, Loader=yaml.FullLoader)
-        
-    while i < num-1:
-        with open(fileList[1]) as file:
-            print("3")
-            toYaml = yaml.load(file, Loader=yaml.FullLoader)
-        fromYaml = merge(fromYaml,toYaml)
-        i = i+1
-    print("4")
-    printing(fromYaml)
-
-def fileToParse(pathlocation):
-    #pathlocation = (r"C:\Users\pumehta\Desktop\Coding challenge\a\Testing.py")
-    fileList = []           #Contains list of files that would be parsed
-    fileList.append(pathlocation)  #Add the provided file into 
-
-    tempPath = os.path.split(pathlocation) 
-    temp = os.path.join(os.path.split(tempPath[0])[0],tempPath[1])
-
-    while (path.isfile(temp)==True):
-        fileList.append(temp)
-        temp2 = os.path.split(os.path.split(temp)[0])[0]
-        temp = os.path.join(temp2[0],tempPath[1])
-    print("2")
-    merging(fileList)
-
+class Test(unittest.TestCase):
     
+    def test_empty_file_inner(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\6\test\test','test6.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','6.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertRaises(TypeError, MergingScript(test1).main())
 
-def main():
-    #pathLocation = input("Please input the path of the child(and last) YAML file to be merged:")
-    pathLocation = (r"C:\Users\pumehta\Desktop\Coding challenge\1.yaml")
-    try:
-        if (path.isfile(pathLocation)==True):
-            print("1")
-            fileToParse(pathLocation)
-            
-        else:
-            print("Error 1: The path provided has an error")
-            return()
+    def test_empty_file_outer(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\7\test\test','test7.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','7.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertRaises(TypeError, MergingScript(test1).main())
+       
+    def test_given_int_and_string(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\1\test','test1.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','1.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertEqual(MergingScript(test1).main(), documents)
 
-    except:
-        print("Error 99: There was an unknown error")
-        return()
+    def test_given_yaml_lists(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\2\test','test2.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','2.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertEqual(MergingScript(test1).main(), documents)
 
+    def test_given_nested_key_value(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\3\test','test3.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','3.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertEqual(MergingScript(test1).main(), documents)
+
+    def test_sister_folder_present(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\4\test\test','test4.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','4.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertEqual(MergingScript(test1).main(), documents)
+
+    def test_complex(self):
+        test1 = os.path.normpath(os.path.join(dirname,r'TestCase\5\test','test5.yaml'))
+        test1_ans = os.path.normpath(os.path.join(dirname,r'TestCase\expected','5.yaml'))
+        with open(test1_ans) as file:
+            documents = yaml.load(file, Loader=yaml.FullLoader)
+        self.assertEqual(MergingScript(test1).main(), documents)
+    
 if __name__== "__main__":
-   main()
-
-
+    dirname = os.path.dirname(__file__)
+    unittest.main()
